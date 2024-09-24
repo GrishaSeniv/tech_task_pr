7bd2bc0f09dae461e2b3e3479af2fb30078fbf98
Hryhorii Seniv <hryhorii.seniv@javatar.pro>
2024-09-24 15:48:42 +0300

    initial commit

---
+++ src/main/java/technical/task/card_order/CardOrderApplication.java
@@ -0,0 +1,15 @@
+package technical.task.card_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Hryhorii Seniv
+ * @version 2024-09-24
+ */
+@SpringBootApplication
+public class CardOrderApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(CardOrderApplication.class, args);
+        }
+}
